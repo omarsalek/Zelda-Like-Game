228221aa5606d9c662afac1460f2871741c027c9
MARCINKOWSKI Jérémy <jeremy.marcinkowski@cnav.fr>
2021-05-06 09:41:28 +0200

    Integration de la branche Omar dans TiledEtControle

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+	
+import java.io.File;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = FXMLLoader.load(getClass().getResource("vue/Zelda-king.fxml"));
+			Scene scene = new Scene(root,800,600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
